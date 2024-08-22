7befcd6f47dc22e84a4d0378ac1c39f29ab2787e
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-08-22 22:24:55 +0600

    initialized new project

---
+++ ghostplay/src/test/java/com/ghost/ghostplay/GhostplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghost.ghostplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
