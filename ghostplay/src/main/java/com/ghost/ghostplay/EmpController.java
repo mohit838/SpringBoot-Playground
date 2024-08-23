3001beb40845e7f327f18412b1c496c62dc9df5b
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-08-23 14:50:08 +0600

    added corsorigin

--- ghostplay/src/main/java/com/ghost/ghostplay/EmpController.java
+++ ghostplay/src/main/java/com/ghost/ghostplay/EmpController.java
@@ -3,12 +3,16 @@ package com.ghost.ghostplay;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("http://localhost:3000")
 @RestController
+@RequestMapping("/api/v1/")
 public class EmpController {
 
     List<Employee> employees = new ArrayList<>();
