c888a2ccb82861cb6360f99bb7495f5c2d4efd90
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-08-25 16:06:15 +0600

    added frontend project

--- ghostplay/src/main/java/com/ghost/ghostplay/EmpController.java
+++ ghostplay/src/main/java/com/ghost/ghostplay/EmpController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("http://localhost:3003")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmpController {
