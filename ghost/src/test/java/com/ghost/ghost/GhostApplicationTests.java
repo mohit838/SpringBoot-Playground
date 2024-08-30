807125ce89d0d529255532ff214cb05dc1c7f377
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-08-30 19:34:51 +0600

    added ghost apps

---
+++ ghost/src/test/java/com/ghost/ghost/GhostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghost.ghost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
