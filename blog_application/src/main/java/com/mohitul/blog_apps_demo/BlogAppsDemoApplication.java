23ab279b8bac53b3d414b8772f692d3adbfdc7c1
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-09-02 10:55:28 +0600

    added blogapplicatoins

---
+++ blog_application/src/main/java/com/mohitul/blog_apps_demo/BlogAppsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mohitul.blog_apps_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppsDemoApplication.class, args);
+	}
+
+}
