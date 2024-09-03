ea361e7d9160082946e6507ee19dc7f4d54b7d74
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-09-03 12:59:17 +0600

    valid exception from controller added in global exceptions

--- blog_application/src/main/java/com/mohitul/blog_apps_demo/exceptions/GlobalExceptionHandler.java
+++ blog_application/src/main/java/com/mohitul/blog_apps_demo/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.mohitul.blog_apps_demo.exceptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,9 +16,25 @@ import com.mohitul.blog_apps_demo.apiResponse.ApiResponse;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException exception) {
+    public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(
+            ResourceNotFoundException exception) {
         String message = exception.getMessage();
         ApiResponse apiResponse = new ApiResponse(message, false);
         return new ResponseEntity<ApiResponse>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgsNotValidExceptions(
+            MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+
+        exception.getBindingResult().getAllErrors().forEach(
+                (e) -> {
+                    String fieldName = ((FieldError) e).getField();
+                    String message = e.getDefaultMessage();
+                    errors.put(fieldName, message);
+                });
+
+        return new ResponseEntity<Map<String, String>>(errors, HttpStatus.BAD_REQUEST);
     }
 }
