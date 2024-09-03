b5667b3e9b04f5cc7d75ee2d9e6c97c972c1b48f
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-09-03 11:55:21 +0600

    user controller complete

--- blog_application/src/main/java/com/mohitul/blog_apps_demo/controller/UserController.java
+++ blog_application/src/main/java/com/mohitul/blog_apps_demo/controller/UserController.java
@@ -1,12 +1,19 @@
 package com.mohitul.blog_apps_demo.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mohitul.blog_apps_demo.apiResponse.ApiResponse;
 import com.mohitul.blog_apps_demo.payloads.UserDto;
 import com.mohitul.blog_apps_demo.services.UserServices;
 
@@ -14,14 +21,43 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/user")
 public class UserController {
     private UserServices userServices;
 
-    @PostMapping("/user")
+    @PostMapping("/")
     public ResponseEntity<UserDto> createNewUser(@RequestBody UserDto userDto) {
         UserDto newUser = userServices.createNewUser(userDto);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDto> updateUser(
+            @RequestBody UserDto userDto, @PathVariable("id") Long userId) {
+        UserDto updatedUser = userServices.updateUser(userDto, userId);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long userId) {
+        UserDto getUserById = userServices.getUserById(userId);
+        return ResponseEntity.ok(getUserById);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        List<UserDto> userLists = userServices.listOfUsers();
+        return ResponseEntity.ok(userLists);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse> deleteEmployeeById(@PathVariable("id") Long userId) {
+        // public ResponseEntity<?> deleteEmployeeById(@PathVariable("id")
+        // Long userId) {
+        userServices.deleteUser(userId);
+        ApiResponse apiResponse = new ApiResponse("Delete User Successfully.", true);
+        // return ResponseEntity.ok(Map.of("message", "User deleted successfully."));
+        return ResponseEntity.ok(apiResponse);
     }
 
 }
