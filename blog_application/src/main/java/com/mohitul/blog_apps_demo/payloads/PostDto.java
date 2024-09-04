cdb5d57c83747b2f6b9572d8d415dd0bffd82447
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-09-04 12:29:10 +0600

    use prettier

--- blog_application/src/main/java/com/mohitul/blog_apps_demo/payloads/PostDto.java
+++ blog_application/src/main/java/com/mohitul/blog_apps_demo/payloads/PostDto.java
@@ -1,13 +1,11 @@
 package com.mohitul.blog_apps_demo.payloads;
 
-import com.mohitul.blog_apps_demo.entity.CategoryEntity;
-import com.mohitul.blog_apps_demo.entity.UserEntity;
+import java.util.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.Date;
 
 @Setter
 @Getter
--- blog_application/src/main/java/com/mohitul/blog_apps_demo/services/PostServices.java
+++ blog_application/src/main/java/com/mohitul/blog_apps_demo/services/PostServices.java
@@ -1,18 +1,24 @@
 package com.mohitul.blog_apps_demo.services;
 
-import com.mohitul.blog_apps_demo.entity.PostEntity;
-import com.mohitul.blog_apps_demo.payloads.PostDto;
-
 import java.util.List;
+
+import com.mohitul.blog_apps_demo.payloads.PostDto;
 
 public interface PostServices {
 
     PostDto createNewPost(PostDto postDto, Long userId, Long categoryId);
+
     PostDto updatePost(PostDto postDto, Long postId);
+
     PostDto getPostById(Long postId);
+
     List<PostDto> getAllPosts();
+
     void deletePost(Long postId);
+
     List<PostDto> getAllPostsByCategoryId(Long categoryId);
+
     List<PostDto> getAllPostsByUserId(Long userId);
+
     List<PostDto> searchPosts(String keyword);
 }
