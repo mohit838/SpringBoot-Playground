8721a3b642a3d54893353f748e88cfe72671950b
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-09-06 16:23:51 +0600

    added cors origin settings

--- blog_application/src/main/java/com/mohitul/blog_apps_demo/config/SecurityConfig.java
+++ blog_application/src/main/java/com/mohitul/blog_apps_demo/config/SecurityConfig.java
@@ -10,41 +10,48 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import com.mohitul.blog_apps_demo.security.JwtAuthenticationEntryPoint;
 import com.mohitul.blog_apps_demo.security.JwtAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
-        private final JwtAuthenticationEntryPoint entryPoint;
-        private final JwtAuthenticationFilter filter;
+    private final JwtAuthenticationEntryPoint entryPoint;
+    private final JwtAuthenticationFilter filter;
 
-        public SecurityConfig(JwtAuthenticationEntryPoint entryPoint, JwtAuthenticationFilter filter) {
-                this.entryPoint = entryPoint;
-                this.filter = filter;
-        }
+    public SecurityConfig(JwtAuthenticationEntryPoint entryPoint, JwtAuthenticationFilter filter) {
+        this.entryPoint = entryPoint;
+        this.filter = filter;
+    }
 
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-                http.csrf(AbstractHttpConfigurer::disable)
-                                .cors(AbstractHttpConfigurer::disable)
-                                .authorizeHttpRequests(auth -> auth
-                                                .requestMatchers(
-                                                                "/test",
-                                                                "/api/v1/auth/login",
-                                                                "/api/v1/auth/refresh")
-                                                .permitAll()
-                                                .requestMatchers(
-                                                                "/api/v1/posts/**",
-                                                                "/api/v1/comments/**",
-                                                                "/api/v1/categories/**")
-                                                .hasAnyRole("ADMIN", "USER")
-                                                .anyRequest().authenticated() // Ensure all other requests are
-                                                                              // authenticated
-                                )
-                                .exceptionHandling(ex -> ex.authenticationEntryPoint(entryPoint))
-                                .sessionManagement(session -> session
-                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                //  .cors(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(request -> {
+                    var corsConfiguration = new CorsConfiguration();
+                    corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
+                    corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+                    corsConfiguration.setAllowedHeaders(List.of("*"));
+                    return corsConfiguration;
+                }))
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers(
+                                        "/api/v1/test",
+                                        "/api/v1/auth/login",
+                                        "/api/v1/auth/refresh").permitAll()
+                                .requestMatchers(
+                                        "/api/v1/posts/**",
+                                        "/api/v1/comments/**",
+                                        "/api/v1/categories/**").hasAnyRole("ADMIN", "USER")
+                                .anyRequest().authenticated()
+                )
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(entryPoint))
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-                http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
-                return http.build();
-        }
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 }
