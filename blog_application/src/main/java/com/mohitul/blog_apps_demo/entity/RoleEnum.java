7e36b03839f035acb56a3e427855acc4e958ed16
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-09-06 16:36:13 +0600

    Creating Enum Types for user roles

---
+++ blog_application/src/main/java/com/mohitul/blog_apps_demo/entity/RoleEnum.java
@@ -0,0 +1,7 @@
+package com.mohitul.blog_apps_demo.entity;
+
+public enum RoleEnum {
+    ADMIN,
+    NORMAL_USER,
+    MODERATOR;
+}
--- blog_application/src/main/java/com/mohitul/blog_apps_demo/entity/UserEntity.java
+++ blog_application/src/main/java/com/mohitul/blog_apps_demo/entity/UserEntity.java
@@ -5,22 +5,11 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -53,7 +42,10 @@ public class UserEntity implements UserDetails {
 
     // TODO:: Make this role to roles
     @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    @Enumerated(EnumType.STRING)
     private Set<Role> roles = new HashSet<>();
 
     @Override
