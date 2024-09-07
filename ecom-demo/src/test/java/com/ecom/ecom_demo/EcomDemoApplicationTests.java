74dfa10510ba15cfca6088a309de9da3cee80bf4
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-09-07 16:14:00 +0600

    old and initials

---
+++ ecom-demo/src/test/java/com/ecom/ecom_demo/EcomDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.ecom_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
